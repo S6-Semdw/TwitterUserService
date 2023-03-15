24c56c50edbffb01b6328b4dbf84b8c8356cfa7f
Sem de Wilde <sem.dewilde6@gmail.com>
2023-03-15 10:35:03 +0100

    Initial commit

---
+++ src/main/java/com/example/twitteruserservice/TwitterUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitteruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twitteruserservice/TwitterUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitteruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
