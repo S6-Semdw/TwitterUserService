858d9f921b7e4a9013e2df089bd1f024ac5c1334
Sem de Wilde <sem.dewilde6@gmail.com>
2023-03-15 17:52:01 +0100

    Added repo

--- src/main/java/com/example/twitteruserservice/repo/UserRepo.java
+++ src/main/java/com/example/twitteruserservice/repo/UserRepo.java
@@ -3,5 +3,6 @@ package com.example.twitteruserservice.repo;
 import com.example.twitteruserservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepo extends JpaRepository<User, Integer> {
 }
