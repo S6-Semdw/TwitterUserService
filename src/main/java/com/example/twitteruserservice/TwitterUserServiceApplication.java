ff2131e5aec58ca9b487d86ca7763ee1295b0d24
Sem de Wilde <sem.dewilde6@gmail.com>
2023-05-31 09:53:05 +0200

    Added more logging

--- src/main/java/com/example/twitteruserservice/TwitterUserServiceApplication.java
+++ src/main/java/com/example/twitteruserservice/TwitterUserServiceApplication.java
@@ -1,14 +1,20 @@
 package com.example.twitteruserservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TwitterUserServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TwitterUserServiceApplication.class, args);
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterUserServiceApplication.class);
 
+    public static void main(String[] args) {
+        LOGGER.info("Starting Twitter User Service Application");
+        SpringApplication.run(TwitterUserServiceApplication.class, args);
+        LOGGER.info("Twitter User Service Application started");
+    }
 }
