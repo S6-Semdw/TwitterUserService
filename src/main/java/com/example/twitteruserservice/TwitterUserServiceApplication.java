07eb82ae5b2133f69aafc7127044fdc361b2af38
Sem de Wilde <sem.dewilde6@gmail.com>
2023-05-31 14:47:20 +0200

    Deleted certain dependencies and files

--- src/main/java/com/example/twitteruserservice/TwitterUserServiceApplication.java
+++ src/main/java/com/example/twitteruserservice/TwitterUserServiceApplication.java
@@ -4,11 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class TwitterUserServiceApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(TwitterUserServiceApplication.class);
 
